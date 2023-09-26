07eeb07b1dc25999a96dfc401d34790e4bd8fa69
ddeweerd <dirkdeweerd.work@gmail.com>
2023-09-26 15:34:35 +0200

    py function

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
   #VAE_builder <- reticulate::import_from_path(module = "VAE_builder", path = path)
 
   path <- system.file(package = "VAEAnalysis")
-  #utils_VAE <- reticulate::import_from_path(module = "utils_VAE", path = path)
+  utils_VAE <- reticulate::import_from_path(module = "utils_VAE", path = path)
 
   #load_model <<- utils_VAE$load_model
   load_model <<- NULL
