bd815bf79495a89838302540445d0987be531fe3
ddeweerd <dirkdeweerd.work@gmail.com>
2023-09-26 15:30:57 +0200

    putting things back in

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+#' @import keras
+#' @import dplyr
+#' @import reticulate
+#' @import magrittr
+#' @import tensorflow
+
+
+#' @export
+load_model <- function(filepath){
+  load_model(filepath)
+}
+
