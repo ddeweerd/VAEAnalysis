698d8e3f1f4f94fd357dc68a95315db290f6fd71
ddeweerd <dirkdeweerd.work@gmail.com>
2023-09-26 17:09:45 +0200

    also reticulate

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,6 @@
 #' @import keras
 #' @import dplyr
-#' @import reticulate
 #' @import magrittr
 #' @import tensorflow
 
-
-#' @export
-load_model <- function(filepath){
-  load_model(filepath)
-}
 
