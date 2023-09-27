62b7bda07cb5eefed98a1fdf83b89aa4e63deaff
ddeweerd <dirkdeweerd.work@gmail.com>
2023-09-27 08:26:03 +0200

    .onLoad back

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,14 @@
-# .onLoad <- function(libname, pkgname){
-#
-#   encoder_path <- system.file("extdata", "encoder.h5", package = "VAEanalysis")
-#   decoder_path <- system.file("extdata", "decoder.h5", package = "VAEanalysis")
-#   vae_path <- system.file("extdata", "VAE.h5", package = "VAEanalysis")
-#
-#   encoder <<- keras::load_model_hdf5(encoder_path)
-#   decoder <<- keras::load_model_hdf5(decoder_path)
-#   vae <<- keras::load_model_hdf5(encoder_path)
-#
-#
-#
-#
-# }
+.onLoad <- function(libname, pkgname){
+
+  encoder_path <- system.file("extdata", "encoder.h5", package = "VAEanalysis")
+  decoder_path <- system.file("extdata", "decoder.h5", package = "VAEanalysis")
+  vae_path <- system.file("extdata", "VAE.h5", package = "VAEanalysis")
+
+  encoder <<- keras::load_model_hdf5(encoder_path)
+  decoder <<- keras::load_model_hdf5(decoder_path)
+  vae <<- keras::load_model_hdf5(encoder_path)
+
+
+
+
+}
