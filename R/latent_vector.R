f986d4753e60ea5f1583999db1f9a297f3ec8513
ddeweerd <dirkdeweerd.work@gmail.com>
2023-10-09 15:39:54 +0200

    changed back to mod, also in lv function1

--- R/latent_vector.R
+++ R/latent_vector.R
@@ -9,7 +9,7 @@ get_condition_latent_vector <- function(patient_counts, control_counts, mod){
 }
 #'@export
 get_latent_space <- function(counts, mod){
-  encoder %>% predict(t(counts))
+  mod$encoder %>% predict(t(counts))
 }
 #' @export
 get_module_genes <- function(rank_vector, max_rank){
