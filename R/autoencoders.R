65bfa6595bedd3decb4dc13bfe7330d3f9c5dd56
Dirk de Weerd <hendrik.de.weerd@liu.se>
2024-09-24 17:01:33 +0200

    changed back to original

--- R/autoencoders.R
+++ R/autoencoders.R
@@ -1,38 +1,32 @@
 #' @export
-build_autoencoder <- function(input_size = 48, layer1 = 24, layer2 = 12, layer3 = 24) {
+build_autoencoder <- function(input_size = 48, layer1 = 24, layer2 = 12, layer3 = 24 ){
 
-  # Encoder
   encoder_input <- layer_input(shape = c(input_size))
   encoder_output <- encoder_input %>%
     layer_dense(units = layer1, activation = 'relu') %>%
     layer_dense(units = layer2, activation = 'relu')
 
-  encoder <- keras_model(inputs = encoder_input, outputs = encoder_output)
 
-  # Decoder
+  encoder <- keras_model(encoder_input, encoder_output)
+
   decoder_input <- layer_input(shape = c(layer2))
   decoder_output <- decoder_input %>%
     layer_dense(units = layer3, activation = 'relu') %>%
     layer_dense(units = input_size)
 
-  decoder <- keras_model(inputs = decoder_input, outputs = decoder_output)
+  decoder <- keras_model(decoder_input, decoder_output)
 
-  # Autoencoder: Connect the encoder and decoder
-  autoencoder_output <- encoder_output %>%
+  autoencoder_output <- encoder_input %>%
+    encoder %>%
     decoder
 
-  autoencoder <- keras_model(inputs = encoder_input, outputs = autoencoder_output)
-  print(class(autoencoder))  # Should be "keras.engine.functional.Functional" or "keras.models.Model"
+  autoencoder <- keras_model(encoder_input, autoencoder_output)
 
-  # Compile the autoencoder
   autoencoder %>% compile(
     optimizer = 'adam',
     loss = 'mean_squared_error'
   )
-
-  return(autoencoder)  # Return the compiled autoencoder model
 }
-
 
 #'@export
 slice_history <- function(history, n_epochs){
